fedba260500074741ecc7134f090b949e59d6e4e
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-09-04 21:20:51 +0200

    created homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">DICTIONARY</header>
+      <div className="card-body">
+        <h5 className="card-title">Look up a word, learn it forever</h5>
+        <p className="card-text">What word do you want to look up?</p>
+        <input type="text" class="form-control" placeholder="Enter a word" />
+      </div>
     </div>
   );
 }
-
-export default App;
