3d9be91e02ee79dc20b85c8350ba144272d65e55
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-09-08 19:24:55 +0200

    api added

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Search() {
   const [keyword, setKeyword] = useState("");
-  function handleSearch(event) {
-    event.preventDefault();
-    console.log(`searching ${keyword}`);
-  }
+
   function updateSearchingWord(event) {
     setKeyword(event.target.value);
   }
+  function handleSearch(event) {
+    event.preventDefault();
+    console.log(`searching ${keyword}`);
+    let dicitionary_api_url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(dicitionary_api_url).then(handleResponse);
+  }
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   return (
     <div className="col-m-6">
       <form onSubmit={handleSearch}>
