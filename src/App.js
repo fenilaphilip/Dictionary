0a6a64a51f64d67528ddbebece536291128a4b81
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-09-12 10:43:25 +0200

    sub-heading added

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ export default function App() {
   return (
     <div className="App container col-m-8">
       <header className="App-header m-4">DICTIONARY</header>
+      <div className="sub-header">English-English</div>
       <div className="card-body m-4">
         <h5 className="card-title mt-4">Look up a word, learn it forever!</h5>
         <p className="card-text mt-4 pt-4">What word do you want to look up?</p>
