bf72b2d100d949100c3065b41520f2640f669b11
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-09-18 12:24:05 +0200

    icon added

--- src/App.js
+++ src/App.js
@@ -74,7 +74,14 @@ export default function App() {
 
   return (
     <div className="App container col-m-8">
-      <header className="App-header m-4">DICTIONARY </header>
+      <header className="App-header m-4">
+        <img
+          src={require("./magnifying-glass-book.png")}
+          alt="logo"
+          className="icon"
+        />
+        DICTIONARY{" "}
+      </header>
 
       <div className="row m-2">
         <div className="col-3">
